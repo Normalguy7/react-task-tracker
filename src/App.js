7dde3ba5807c7fb438ea1c73f6d8071dae3b8e3c
jay <jay.13.97@gmail.com>
2021-07-27 19:39:46 +1200

    react task list app

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import Header from "./Components/Header";
+import Tasks from "./Components/Tasks";
+
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2.30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'School Meeting',
+      day: 'Feb 2nd at 4.30pm',
+      reminder: false,
+    },
+  ])
+
+  // delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  return (
+    <div className="container">
+      <Header />
+      {tasks.length > 0 ? ( <Tasks tasks={tasks} onDelete={deleteTask} /> ) : ('No tasks to show')}
+    </div>
+  );
+}
+
+export default App;
